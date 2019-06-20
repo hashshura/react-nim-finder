1c2478610b792eeddd97add7951801be358049d2
Asif Hummam Rais <hashshura@gmail.com>
2019-06-20 23:59:01 +0700

    Updates

---
+++ precache-manifest.1cb0308c343a0e8386f428d9de77bc2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7b119ccf27e198a9afb3f96912cda1",
+    "url": "/react-nim-finder/index.html"
+  },
+  {
+    "revision": "f3bf4b605d9b33aa3ddc",
+    "url": "/react-nim-finder/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ad75b7cf9adccc3af33c",
+    "url": "/react-nim-finder/static/js/2.4ea86682.chunk.js"
+  },
+  {
+    "revision": "f3bf4b605d9b33aa3ddc",
+    "url": "/react-nim-finder/static/js/main.e7a7534a.chunk.js"
+  },
+  {
+    "revision": "aa81f99b8f8b70a655cf",
+    "url": "/react-nim-finder/static/js/runtime~main.bbf67bd6.js"
+  }
+]);
