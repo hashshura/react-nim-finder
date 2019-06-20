9cc98c49290aa5202ca6a459527d8f6d732af8d6
Asif Hummam Rais <hashshura@gmail.com>
2019-06-20 21:24:53 +0700

    Add register and login components

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,167 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Redirect } from "react-router";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
+import { withStyles } from "@material-ui/core/styles";
+import { styles } from "./styles";
+
+import postApiLogin from "libs/postApiLogin";
+
+import {
+  K_ROUTE_REGISTER,
+  K_ROUTE_BY_NAME,
+  K_CODE_LOGIN_SUCCESS
+} from "utils/constants";
+
+class LoginForm extends React.Component {
+  state = {
+    username: null,
+    password: null,
+    helperText: ""
+  };
+
+  handleSubmit() {
+    const { setToken } = this.props;
+    const { username, password } = this.state;
+    postApiLogin(username, password).then(response => {
+      const isLoginSuccess = response.code === K_CODE_LOGIN_SUCCESS;
+      setToken(isLoginSuccess ? response.token : "");
+      this.setState({ helperText: isLoginSuccess ? "" : response.status });
+    });
+  }
+
+  renderTitle() {
+    const { classes } = this.props;
+    return (
+      <React.Fragment>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h6">
+          ITB NIM Finder
+        </Typography>
+        <Typography component="h2" variant="h4">
+          Login
+        </Typography>
+      </React.Fragment>
+    );
+  }
+
+  renderForm() {
+    const { helperText } = this.state;
+    const { classes } = this.props;
+    return (
+      <React.Fragment>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          autoComplete="username"
+          autoFocus
+          onChange={event => {
+            this.setState({ username: event.target.value });
+          }}
+          onKeyPress={event => {
+            if (event.key === 'Enter') {
+              this.handleSubmit();
+              event.preventDefault();
+            }
+          }}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          onChange={event => {
+            this.setState({ password: event.target.value });
+          }}
+          onKeyPress={event => {
+            if (event.key === 'Enter') {
+              this.handleSubmit();
+              event.preventDefault();
+            }
+          }}
+          helperText={helperText}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+          onClick={() => this.handleSubmit()}
+        >
+          Login
+        </Button>
+        <Grid container direction="column" alignItems="center">
+          <Grid item>
+            <Link href={K_ROUTE_REGISTER} variant="body2">
+              {"Haven't had an account yet? Register"}
+            </Link>
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+
+  renderLove() {
+    return (
+      <Box mt={5}>
+        <Typography variant="body2" color="textSecondary" align="center">
+          {"Made with love by Asif using "}
+          <Link color="inherit" href="https://material-ui.com/">
+            Material-UI
+          </Link>
+          {" components."}
+        </Typography>
+      </Box>
+    );
+  }
+
+  render() {
+    const { classes, getToken } = this.props;
+    const hasValidToken = getToken() !== "";
+    
+    return hasValidToken ? (
+      <Redirect to={K_ROUTE_BY_NAME} />
+    ) : (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          {this.renderTitle()}
+          {this.renderForm()}
+        </div>
+        {this.renderLove()}
+      </Container>
+    );
+  }
+}
+
+LoginForm.propTypes = {
+  classes: PropTypes.object.isRequired,
+  setToken: PropTypes.func.isRequired,
+  getToken: PropTypes.func.isRequired
+};
+
+export default withStyles(styles)(LoginForm);
---
+++ src/components/RegisterForm/index.js
@@ -0,0 +1,169 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Redirect } from "react-router";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
+import { withStyles } from "@material-ui/core/styles";
+import { styles } from "./styles";
+
+import postApiRegister from "libs/postApiRegister";
+
+import {
+  K_ROUTE_LOGIN,
+  K_ROUTE_BY_NAME,
+  K_CODE_REGISTER_SUCCESS
+} from "utils/constants";
+
+class RegisterForm extends React.Component {
+  state = {
+    username: null,
+    password: null,
+    helperText: ""
+  };
+
+  handleSubmit() {
+    const { username, password } = this.state;
+    postApiRegister(username, password).then(response => {
+      const isRegisterSuccess = response.code === K_CODE_REGISTER_SUCCESS;
+      this.setState({
+        helperText: isRegisterSuccess
+          ? "Registration successful! Login to continue"
+          : response.status
+      });
+    });
+  }
+
+  renderTitle() {
+    const { classes } = this.props;
+    return (
+      <React.Fragment>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h6">
+          ITB NIM Finder
+        </Typography>
+        <Typography component="h2" variant="h4">
+          Register
+        </Typography>
+      </React.Fragment>
+    );
+  }
+
+  renderForm() {
+    const { helperText } = this.state;
+    const { classes } = this.props;
+    return (
+      <React.Fragment>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          autoComplete="username"
+          autoFocus
+          onChange={event => {
+            this.setState({ username: event.target.value });
+          }}
+          onKeyPress={event => {
+            if (event.key === 'Enter') {
+              this.handleSubmit();
+              event.preventDefault();
+            }
+          }}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          onChange={event => {
+            this.setState({ password: event.target.value });
+          }}
+          onKeyPress={event => {
+            if (event.key === 'Enter') {
+              this.handleSubmit();
+              event.preventDefault();
+            }
+          }}
+          helperText={helperText}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="secondary"
+          className={classes.submit}
+          onClick={() => this.handleSubmit()}
+        >
+          Register
+        </Button>
+        <Grid container direction="column" alignItems="center">
+          <Grid item>
+            <Link href={K_ROUTE_LOGIN} variant="body2">
+              {"Already have an account? Login"}
+            </Link>
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+
+  renderLove() {
+    return (
+      <Box mt={5}>
+        <Typography variant="body2" color="textSecondary" align="center">
+          {"Made with love by Asif using "}
+          <Link color="inherit" href="https://material-ui.com/">
+            Material-UI
+          </Link>
+          {" components."}
+        </Typography>
+      </Box>
+    );
+  }
+
+  render() {
+    const { classes, getToken } = this.props;
+    const hasValidToken = getToken() !== "";
+    
+    return hasValidToken ? (
+      <Redirect to={K_ROUTE_BY_NAME} />
+    ) : (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          {this.renderTitle()}
+          {this.renderForm()}
+        </div>
+        {this.renderLove()}
+      </Container>
+    );
+  }
+}
+
+RegisterForm.propTypes = {
+  classes: PropTypes.object.isRequired,
+  setToken: PropTypes.func.isRequired,
+  getToken: PropTypes.func.isRequired
+};
+
+export default withStyles(styles)(RegisterForm);
