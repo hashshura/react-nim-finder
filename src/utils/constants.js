72cfaaea58d52c74317868b4f281a06341a45e99
Asif Hummam Rais <hashshura@gmail.com>
2019-06-20 23:52:32 +0700

    Finalize constants and routing

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,14 +2,16 @@ export const K_LOCAL_STORAGE_TOKEN_ID = 'nim-finder-token';
 
 export const K_ROUTE_REGISTER = '/register';
 export const K_ROUTE_LOGIN = '/login';
-export const K_ROUTE_BY_NAME = '/byname';
-export const K_ROUTE_BY_ID = '/byid';
+export const K_ROUTE_SEARCH = '/';
+
+export const K_API_ROUTE_BY_NAME = '/byname';
+export const K_API_ROUTE_BY_ID = '/byid';
 
 export const K_API_URL = 'https://api.stya.net/nim';
 export const K_API_REGISTER = K_API_URL + K_ROUTE_REGISTER;
 export const K_API_LOGIN = K_API_URL + K_ROUTE_LOGIN;
-export const K_API_BY_NAME = K_API_URL + K_ROUTE_BY_NAME;
-export const K_API_BY_ID = K_API_URL + K_ROUTE_BY_ID;
+export const K_API_BY_NAME = K_API_URL + K_API_ROUTE_BY_NAME;
+export const K_API_BY_ID = K_API_URL + K_API_ROUTE_BY_ID;
 
 export const K_CODE_REGISTER_USERNAME_TAKEN = -4;
 export const K_CODE_REGISTER_MISSING_PARAMETER = -2;
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,50 +1,25 @@
 import {
   K_ROUTE_REGISTER,
   K_ROUTE_LOGIN,
-  K_ROUTE_BY_NAME,
-  K_ROUTE_BY_ID,
-  K_LABEL_BY_NAME,
-  K_LABEL_BY_ID
 } from "utils/constants";
 
 import LoginForm from "components/LoginForm";
 import RegisterForm from "components/RegisterForm";
 import Searcher from "components/Searcher";
 
-import getApiByName from "libs/getApiByName";
-import getApiById from "libs/getApiById";
-
 const routes = [
   {
     path: K_ROUTE_LOGIN,
     component: LoginForm,
-    props: {}
   },
   {
     path: K_ROUTE_REGISTER,
     component: RegisterForm,
-    props: {}
   },
   {
-    path: K_ROUTE_BY_NAME,
+    /** Default Route */
     component: Searcher,
-    props: {
-      routeOther: K_ROUTE_BY_ID,
-      getApiFunction: getApiByName,
-      label: K_LABEL_BY_NAME,
-      labelOther: K_LABEL_BY_ID
-    }
   },
-  {
-    path: K_ROUTE_BY_ID,
-    component: Searcher,
-    props: {
-      routeOther: K_ROUTE_BY_NAME,
-      getApiFunction: getApiById,
-      label: K_LABEL_BY_ID,
-      labelOther: K_LABEL_BY_NAME
-    }
-  }
 ];
 
 export default routes;
